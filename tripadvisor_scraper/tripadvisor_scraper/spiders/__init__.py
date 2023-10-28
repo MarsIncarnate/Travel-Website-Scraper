370f2db1fd5b41606f1096c123d06e5acf052321
MarsIncarnate <marvellousadeogun@gmail.com>
2023-10-28 20:34:59 +0100

    add scraper for tripadvisor which writes data in csv file

--- tripadvisor_scraper/tripadvisor_scraper/spiders/__init__.py
+++ tripadvisor_scraper/tripadvisor_scraper/spiders/__init__.py
@@ -1,4 +1,5 @@
 import scrapy
+import csv
 
 class TripadvisorReviewsSpider(scrapy.Spider):
     name = "tripadvisor_reviews"
@@ -6,13 +7,42 @@ class TripadvisorReviewsSpider(scrapy.Spider):
         'https://www.tripadvisor.com/Restaurants-g60713-San_Francisco_California.html',
     ]
 
-    def parse(self, response):
-        # Extract review data (e.g., reviewer name, text, rating)
-        review_items = response.xpath('//div[@data-test-target="reviews-tab"]')
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.50 Safari/537.36',
+    }
 
-        for item in review_items:
-            yield {
-                'reviewer_name': item.xpath('.//div[contains(@class, "info_text")]/a/text()').get(),
-                'review_text': item.xpath('.//q/span/text()').get(),
-                'rating': item.xpath('.//span[contains(@class, "ui_bubble_rating")]/@class').re_first(r'\d+'),
-            }
+    def parse(self, response):
+        # Click the anchor element to load all reviews
+        for restaurant in response.css('.BMQDV, .FGwzt, .ukgoS'):
+            yield response.follow(restaurant, self.parse_reviews)
+
+    def parse_reviews(self, response):
+        # Target the review container based on its ID
+        reviews_head = response.xpath('//div[@id="taplc_location_reviews_list_resp_rr_resp_0"]')
+        
+        reviews = reviews_head.xpath('//div[contains(@class, "listContainer")]')
+        
+
+        # Initialize the CSV file and write header
+        with open('tripadvisor_reviews.csv', 'a', newline='') as csvfile:
+            fieldnames = ['reviewer_name', 'review_text', 'rating']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            # Iterate through each review and write to the CSV
+            for review in reviews:
+                reviewer_name = review.xpath('.//div[contains(@class, "info_text")]//div/text()').get()
+                print(reviewer_name)
+                review_text = review.xpath('.//p[contains(@class, "partial_entry")]/text()').get()
+                print(review_text)
+                rating = review.xpath('.//span[contains(@class, "ui_bubble_rating bubble_")]/@class').re_first(r'\d+')
+                print(rating)
+
+                # Check if any of the data is missing before writing to the CSV
+                if reviewer_name and review_text and rating:
+                    writer.writerow({
+                        'reviewer_name': reviewer_name,
+                        'review_text': review_text,
+                        'rating': rating,
+                    })
+
